24e2bb0adc85815b42d2506d803122ccdaf74ba2
Al3xand3r11 <alexander.nance11@gmail.com>
2024-06-19 15:09:01 -0700

    pages mobile fix

--- src/components/pages/Seekers/BottomSeekers.jsx
+++ src/components/pages/Seekers/BottomSeekers.jsx
@@ -6,9 +6,9 @@ import { Link } from 'react-router-dom'
 
 const BottomSeekers = () => {
     return (
-        <div className="bg-white h-screen content-center">
+        <div className="bg-white h-auto content-center">
             <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl text-center">Sectors</h1>
-            <div className="grid grid-cols-4 gap-4 justify-between pl-10 pr-10 pt-16">
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-4 justify-between pl-10 pr-10 pt-16">
                 <div>
                     <img className='mx-auto pb-8' src={Software} width={50} height={50} />
                     <h2 className='text-center font-extrabold'>IT and Software</h2>
@@ -32,7 +32,7 @@ const BottomSeekers = () => {
                     <p className='text-center'>We can assist if you are a pet/child-care provider, a personal assistant, a chef/caterer, or an estate manager.</p>
                 </div>
             </div>
-            <div className='text-center pt-24'>
+            <div className='text-center pt-24 pb-8'>
             <Link to='/Contact' className='inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center border border-black rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900'>Get Started</Link>
             </div>
         </div>
--- src/components/pages/TMinus/TopSection.jsx
+++ src/components/pages/TMinus/TopSection.jsx
@@ -2,16 +2,16 @@ import Terra from '../../../assets/terra.jpeg'
 
 const TopSection = () => {
     return (
-        <div className="bg-white h-screen">
-            <div className="flex flex-row pt-48 pl-8 pr-8">
-                <div className='flex-initial w-8/12 pr-8'>
-                    <h2 className='font-bold text-3xl'>
+        <div className="bg-white h-auto">
+            <div className="flex flex-col-reverse md:flex-row pt-48 pl-8 pr-8">
+                <div className='flex-initial md:w-8/12 pr-8'>
+                    <h2 className='font-bold text-xl md:text-3xl'>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi leo urna molestie at. 
                         Hendrerit gravida rutrum quisque non tellus orci. Vel elit scelerisque mauris pellentesque. Iaculis eu non diam phasellus vestibulum lorem sed risus 
                         ultricies. Id cursus metus aliquam eleifend mi. <br/> 
                     </h2>
                 </div>
-                <div className='flex-initial w-3/12'>
+                <div className='flex-initial w-5/12 md:w-3/12 pb-8'>
                     <img src={Terra} width="1000" height="1000" className='rounded-full'/>
                 </div>
             </div>
